a08d8f3c13f45629506a36d49c9d1c490a40b7c1
yusufKaymaz <kaymazyusuf88@gmail.com>
2022-12-15 12:50:46 +0300

    date added

--- src/App.js
+++ src/App.js
@@ -2,23 +2,37 @@ import { useEffect,useState } from "react"
 import io from "socket.io-client"
 const socket = io.connect("http://localhost:3001")
 function App() {
+  
+  // console.log(messageReceived);
   const [message, setMessage] = useState("")
-  const [messageReceived, setMessageReceived] = useState("")
+  const [messageReceived, setMessageReceived] = useState([])
+  const [myMessages, setMyMessages] = useState([])
   const sendMessage =()=>{
     socket.emit("send_message", {message})
+    setMyMessages([...myMessages,message])
   }
   useEffect(() => {
     socket.on("receive_message", (data)=>{
-      setMessageReceived(data.message)
+      // console.log(data)
+      setMessageReceived([...messageReceived, data])
     })
-  }, [socket])
+  }, [messageReceived])
+
+  // console.log(messageReceived)
+  console.log(myMessages)
+  // console.log("render");
   
+ 
   return (
     <div className="app">
      <input placeholder="Send a message..." type="text" onChange={(e)=>setMessage(e.target.value)} />
      <button onClick={sendMessage}>Send</button>
      <h1>Message:</h1>
-     {messageReceived}
+     {messageReceived.map((message, index)=>{
+      return (
+        <p key={index}>{message.data.message}</p>
+      )
+     })}
     </div>
   );
 }
