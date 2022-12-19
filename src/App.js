330235b58c4791c8e7a754dd112a16eaf86456b9
yusufKaymaz <kaymazyusuf88@gmail.com>
2022-12-19 09:21:30 +0300

    in order to add room system previous codes removed

--- src/App.js
+++ src/App.js
@@ -6,21 +6,18 @@ function App() {
   // console.log(messageReceived);
   const [message, setMessage] = useState("")
   const [messageReceived, setMessageReceived] = useState([])
-  const [myMessages, setMyMessages] = useState([])
+
   const sendMessage =()=>{
     socket.emit("send_message", {message})
-    setMyMessages([...myMessages,message])
   }
   useEffect(() => {
     socket.on("receive_message", (data)=>{
       // console.log(data)
-      setMessageReceived([...messageReceived, data])
+      setMessageReceived(data)
     })
   }, [messageReceived])
 
-  // console.log(messageReceived)
-  console.log(myMessages)
-  // console.log("render");
+
   
  
   return (
@@ -28,11 +25,7 @@ function App() {
      <input placeholder="Send a message..." type="text" onChange={(e)=>setMessage(e.target.value)} />
      <button onClick={sendMessage}>Send</button>
      <h1>Message:</h1>
-     {messageReceived.map((message, index)=>{
-      return (
-        <p key={index}>{message.data.message}</p>
-      )
-     })}
+     <p>{messageReceived.message}</p>
     </div>
   );
 }
